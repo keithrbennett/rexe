7d8eefb45430cbfbf1fb6b7be1561006a4742ee1
Keith Bennett <keithrbennett@gmail.com>
2020-04-23 16:39:36 -0400

    Mark entire file frozen_string_literal: true.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,17 @@ require "bundler/setup"
 REXE_FILE = File.join(File.dirname(__FILE__), '..', 'exe', 'rexe')
 
 # Use this so that testing rexe with requires not in the bundle will load successfully:
-RUN = ->(command) { Bundler.with_clean_env { `#{command}` } }
+RUN = ->(command) do
+  # This used to be an unconditional call to with_clean_env but that method is now deprecated:
+  # [DEPRECATED] `Bundler.with_clean_env` has been deprecated in favor of `Bundler.with_unbundled_env`.
+  # If you instead want the environment before bundler was originally loaded,
+  # use `Bundler.with_original_env`
+  if Bundler.respond_to?(:with_unbundled_env)
+    Bundler.with_unbundled_env { `#{command}` }
+  else
+    Bundler.with_clean_env { `#{command}` }
+  end
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
